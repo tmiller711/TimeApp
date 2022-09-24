548c6a830dfa92d44c425b735ba81f8b14b6ad4a
Tristin <tmille2004@gmail.com>
2022-09-23 19:36:24 -0500

    adds context processor for time

---
+++ main/context_processors.py
@@ -0,0 +1,9 @@
+import pytz
+from datetime import datetime
+from .utils import get_timezone
+
+def time(request):
+    cur_time = pytz.timezone(get_timezone(request)) 
+    standard_time = datetime.now(cur_time).strftime("%I:%M%p")
+
+    return {'time': standard_time}
